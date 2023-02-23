2b9c9158cf796c9ede7a15765283b7946e8c9e16
Anderson Tiban <andytiban12@gmail.com>
2023-02-23 10:44:25 -0500

    added test and static property method]

--- Bag.js
+++ Bag.js
@@ -1,10 +1,15 @@
 class Bag {
+    static MAXWEIGHT = 23;
+
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
-        } 
+        }else if(weight > Bag.MAXWEIGHT){
+            throw new Error("Bag is over weight");
+        }
         this.weight = weight
     }
 }
+
 
 module.exports = Bag
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,20 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(23)
+        expect(bag.weight).toBe(23)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+    //test bag static property
+    test("static property MAXWEIGHT", ()=>{
+        expect(Bag.MAXWEIGHT).toBe(23);
+    })
+    //test if bag overweight
+    test("bag overweight error",()=>{
+      
+        expect(() => new Bag(50)).toThrowError("Bag is over weight");
     })
 })
